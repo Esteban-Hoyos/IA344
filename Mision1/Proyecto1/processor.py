4482ea660d6efc2ead65f135f24c9c8e915d8a05
Esteban <esteban.hoyos46650@ucaldas.edu.co>
2026-01-17 09:44:09 -0500

    Lector archivos de excel completo

--- Mision1/Proyecto1/processor.py
+++ Mision1/Proyecto1/processor.py
@@ -34,5 +34,24 @@ def process_excel(path):
         ws[f"B{row}"].value,
         ws[f"c{row}"].value
          )
+    #Guardar los cambios en el mismo archivo
+    wb.save(path)
+
+def process_excel_safe(path):
+    try:
+        process_excel(path)
+        return True, "archivo procesado con éxito"
+    except PermissionError:
+        return(
+            False,
+            "El archivo Excel está abierto.\n"
+            "Por favor, cierre el archvio e intente nuevamente"
+        )
+    except KeyError:
+        return False, "Hoja `Datos` no encontrada"
+    except Exception as e:
+        return False,f"Error inesperado:{str(e)}"
+    
+
 
 
